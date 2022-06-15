4cd0b4873ac67a0eba2ee7d9c29271f2e2e6018d
muzankibu977@gmail.com <muzankibu977@gmail.com>
2022-06-15 23:05:28 +0600

    How to comment in Java? check here!

---
+++ Codes/Cd3.java
@@ -0,0 +1,13 @@
+import javax.management.MalformedObjectNameException;
+
+public class Cd3 {
+    public static void main(String[] args) {
+        //This is a single comment
+        System.out.println("I am Java");
+        /*This is multiple
+         * line comment
+         * Hey there
+         * Hello Java
+         */
+    }
+}
