772d4395be8fc4277172da0c1dd371855b58222d
muzankibu977@gmail.com <muzankibu977@gmail.com>
2022-06-15 22:59:20 +0600

    Escape Sequances!

---
+++ Codes/Cd2.java
@@ -0,0 +1,11 @@
+public class Cd2 {
+    public static void main(String[] args) {
+        System.out.println("I am \nJava");
+        System.out.println("I am \tJava");
+        System.out.println("I am \bJava");
+        System.out.println("\\I am Java\\");
+        System.out.println("\'I am Java\'");
+        System.out.println("\"I am Java\"");
+
+    }
+}
