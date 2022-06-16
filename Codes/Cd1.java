0a846da3e670d2d485566fce47b803ce900d510a
muzankibu977@gmail.com <muzankibu977@gmail.com>
2022-06-16 22:53:06 +0600

    First Java code- A simple Biodata!

--- Codes/Cd1.java
+++ Codes/Cd1.java
@@ -2,6 +2,6 @@ public class Cd1 {
     public static void main(String[] args) {
         System.out.println("I am showrav");
         System.out.println("ID: 2021682642");
-        System.out.println("Current Work Domain: Java");
+        System.out.println("Current Work Domain: Java and only Java");
 }
     }
