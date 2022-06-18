ebaa2249e16f7f4d3fc2f0d3bcb9c982c6858846
muzankibu977@gmail.com <muzankibu977@gmail.com>
2022-06-18 19:46:33 +0600

    Format Specifier!

---
+++ Codes/Cd5.java
@@ -0,0 +1,20 @@
+public class Cd5 {
+    public static void main(String[] args) {
+        boolean b= true;//dynamic initialization
+        char c= 'A';//dynamic initialization
+        short s;
+        int i;
+        float f;
+        double d;
+        s= 32500;
+        i= 100000;
+        f= 20.2002f;
+        d= 20.50;
+        System.out.printf("Boolean= %b\n",b);
+        System.out.printf("Character= %c\n",c);
+        System.out.printf("Short= %s\n",s);
+        System.out.printf("Intiger= %d\n",i);
+        System.out.printf("Float= %.3f\n",f);
+        System.out.printf("Double= %f\n",d);
+    }
+}
