36b75b6fd2967db4c34af1bbd9fa78a88a473717
muzankibu977@gmail.com <muzankibu977@gmail.com>
2022-06-18 21:03:59 +0600

    Taking Input from user and printing it!

--- Codes/Cd6.java
+++ Codes/Cd6.java
@@ -6,11 +6,15 @@ public class Cd6 {
         Scanner input= new Scanner(System.in);
         int n1;
         String name;
+        boolean boole;;
         System.out.printf("Enter your name: ");
         name= input.nextLine();
-        System.out.printf("Welcome "+name);
+        System.out.printf("Welcome "+name+"!");
         System.out.printf("\nEnter your number: ");
         n1= input.nextInt();
         System.out.printf("Your number= %d", n1);
+        System.out.printf("\nWhats your boolean input: ");
+        boole= input.nextBoolean();
+        System.out.printf("Your given boolean input: %b",boole);
     }
 }
