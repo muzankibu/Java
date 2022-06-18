acad6c126f89349270510df9e7109c8d09667d96
muzankibu977@gmail.com <muzankibu977@gmail.com>
2022-06-18 20:52:03 +0600

    Taking Input from user and printing it!

--- Codes/Cd6.java
+++ Codes/Cd6.java
@@ -1,10 +1,15 @@
 import java.util.Scanner;
 
+
 public class Cd6 {
     public static void main(String[] args) {
         Scanner input= new Scanner(System.in);
         int n1;
-        System.out.printf("Enter your number: ");
+        String name;
+        System.out.printf("Enter your name: ");
+        name= input.nextLine();
+        System.out.printf("Welcome "+name);
+        System.out.printf("\nEnter your number: ");
         n1= input.nextInt();
         System.out.printf("Your number= %d", n1);
     }
