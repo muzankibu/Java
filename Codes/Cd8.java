c62cd5566ca9c24267d2dc8bae96d6bc93ce82c8
muzankibu977@gmail.com <muzankibu977@gmail.com>
2022-06-18 22:37:17 +0600

    Assignment Operator!

---
+++ Codes/Cd8.java
@@ -0,0 +1,16 @@
+public class Cd8 {
+    public static void main(String[] args) {
+        int a=3,b=5;
+        
+        a+=b;
+        System.out.println("a= "+a);
+         a-=b;
+        System.out.println("a= "+a);
+         a*=b;
+        System.out.println("a= "+a);
+         a/=b;
+        System.out.println("a= "+a);
+         a%=b;
+        System.out.println("a= "+a);
+    }
+}
