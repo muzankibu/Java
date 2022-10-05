a760ef63b536bd5505f6503589528b417a6f873b
muzankibu <muzankibu977@gmail.com>
2022-10-05 10:28:14 +0600

    Math Library Class

---
+++ RandomNumber.java
@@ -0,0 +1,11 @@
+import java.lang.Math;
+public class RandomNumber {
+    public static void main(String[] args) {
+        int max=1000;
+        int min=9999;
+        int x=(int) (Math.random()*(max-min+1)+min);
+        double y=Math.random()*(max-min+1)+min;
+        System.out.println(x);
+        System.out.println(y);
+    }
+}
